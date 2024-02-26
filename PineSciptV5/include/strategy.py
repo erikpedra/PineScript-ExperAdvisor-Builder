2c739e7b95d9a5109e04b8cb9c9e9ff3ec184046
erikpedra <56580637+erikpedra@users.noreply.github.com>
2024-02-26 12:28:07 +0700

    Create strategy.py

---
+++ PineSciptV5/include/strategy.py
@@ -0,0 +1,32 @@
+# By: Erik Pedra
+class Strategy:
+    """
+    Class representing a trading strategy.
+
+    This class provides methods for defining trading strategies
+    and executing trading actions such as opening and closing positions.
+
+    Attributes:
+        None
+    """
+
+    @staticmethod
+    def __params_to_string(**kwargs):
+        """
+        Convert parameters to a string representation.
+
+        Args:
+            kwargs (dict): Dictionary containing parameters.
+
+        Returns:
+            str: String representation of parameters.
+        """
+        return ", ".join([f"{key}={repr(value)}" for key, value in kwargs.items() if value is not None])
+
+    @staticmethod
+    def indicator()
+        return f"buy"
+
+    @staticmethod
+    def strategy():
+        return f"buy"
